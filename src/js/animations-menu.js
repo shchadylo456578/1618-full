3c8b7dc186d8df8ae7cf60c9696071f61046bf1a
shchadylo456578 <tarasdidg@gmail.com>
2017-10-27 15:55:19 +0300

    add :hover on page

--- src/js/animations-menu.js
+++ src/js/animations-menu.js
@@ -1,23 +1,23 @@
 (function(){
 
     var lastScrollTop = 0;
-    
+
     $(window).scroll(function(event){
-       
+
        var st = $(this).scrollTop();
 
         /*
         *
         *   MENU
-        * 
-        */ 
+        *
+        */
 
-       var menu_bottom_line_show = $('.footer').position().top - $(window).height()/2;
+       var menu_bottom_line_show = $('.footer').position().top - $(window).height()/4;
 
        if(st  > menu_bottom_line_show){
 
             $('#navbartitle').show();
-       
+
         }else{
 
             if (st > lastScrollTop){
@@ -25,14 +25,14 @@
                 $('#navbartitle').hide(500);
             } else {
                // upscroll code
-               $('#navbartitle').show();
+               $('#navbartitle').show(300);
             }
 
         }
 
         /*
-         *  Always hide menu list on scroll 
-         * 
+         *  Always hide menu list on scroll
+         *
          */
 
         $('#navbar').hide(500);
@@ -40,34 +40,38 @@
 
 
         lastScrollTop = st;
-    
-        
-    
-    
-    
-    
+
+
+
+
+
+
     });
-
-   
-
-        
-
- 
-
     /*
      *
      *  Menu button
-     * 
-     * 
-     */ 
+     *
+     *
+     */
 
     $('#navbartitle').click(function(e){
-        $('#navbar').show();
+        $('#navbar').show(500);
         $('#navbartitle').hide(500);
     });
 
-
-
-
+    /**
+     *
+     *
+     *
+     * chenge color menu* */
+    // $('.navigation').click(function () {
+    //   if ($(this).hasClass('changeColorText')) {
+    //     $(this).removeClass('changeColorText');
+    //   } else {
+    //     $('.navigation').removeClass('changeColorText');
+    //     $(this).addClass('changeColorText');
+    //
+    //   }
+    // });
 })();
 
--- src/js/dots-animate.js
+++ src/js/dots-animate.js
@@ -14,10 +14,10 @@ $(document).ready(function(){
 
     this.obj.style.top = (container.height() * Math.random()) + 'px';
     this.obj.style.left = (container.width() * Math.random()) + 'px';
-    
+
     // this.size = Math.floor(0 * Math.random()) + 8;
     // this.size = 8;
-    
+
     // this.obj.style.height =  this.size + 'px';
     // this.obj.style.width = this.size + 'px';
 
@@ -29,8 +29,8 @@ $(document).ready(function(){
 
 //   freshDot();
   var dot = [];
-  
-  for(var i = 0 ; i < 8 ; i++ ){
+
+  for(var i = 0 ; i < 9 ; i++ ){
     dot.push(new freshDot());
   }
 
