3c358ea12d69346d95f3e522360a7790e47c73f8
shchadylo456578 <tarasdidg@gmail.com>
2017-10-31 13:16:51 +0200

    add slider not finish

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,33 +1,52 @@
-// var btn_prev = document.getElementById('prev');
-// var btn_next = document.getElementById('next');
-// var images = document.getElementsByClassName('image-slide');
-// var i = 0;
-//
-// btn_prev.addEventListener('click', function() {
-//   images[i].classList.remove("active");
-//   i--;
-//   if(i < 0) {
-//     i = images.length - 1;
-//   }
-//   images[i].classList.add('active');
-// });
-//
-// btn_next.addEventListener('click', function() {
-//   images[i].classList.remove("active");
-//   i++;
-//   //console.log(i);
-//   if(i >= images.length) {
-//     i = 0;
-//   }
-//   images[i].classList.add('active');
-// });
+var btn_prev = document.getElementById('prev');
+var btn_next = document.getElementById('next');
+var images = document.getElementsByClassName('image-slide');
+var i = 0;
 
 
-// function myFunction() {
-//   var x = document.getElementById('myDIV');
-//   if (x.style.display === 'none') {
-//     x.style.display = 'block';
-//   } else {
-//     x.style.display = 'none';
-//   }
-// }
+btn_next.addEventListener('click', function() {
+  images[i].classList.remove(".active");
+  i--;
+  if(i < 0) {
+    i = images.length - 1;
+  }
+  images[i].classList.add('.active');
+  TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 800, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)" });
+
+
+});
+
+btn_prev.addEventListener('click', function() {
+
+  images[i].classList.remove("active");
+  i++;
+  //console.log(i);
+  if(i >= images.length) {
+    i = 0;
+  }
+  images[i].classList.add('active');
+  TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 0, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
+
+});
+
+
+// (function($) {
+//   var btn_prev = $('#prev');
+//   var btn_next = $('#next');
+//   var images = $('.image-slide');
+//   var i = 0;
+//
+//   $(btn_prev).click(function () {
+//     TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"0px 0px 40px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 650, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
+//
+//   });
+//   $(btn_next).click(function () {
+//     TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"0px 0px 40px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 0, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
+//
+//   });
+// })(jQuery);
+
+
+
+
+
