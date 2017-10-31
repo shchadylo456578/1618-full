ebeaacdc3687fa43e3f7ada4f46b603677b373b4
shchadylo456578 <tarasdidg@gmail.com>
2017-10-31 21:16:35 +0200

    add footer vw 100%

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,12 +5,12 @@ var i = 0;
 
 
 btn_next.addEventListener('click', function() {
-  images[i].classList.remove(".active");
+  images[i].classList.remove("active");
   i--;
   if(i < 0) {
     i = images.length - 1;
   }
-  images[i].classList.add('.active');
+  images[i].classList.add('active');
   TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {x: 800, scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)" });
 
 
