5234bb5dafec25a560647f748286d482f5d615a4
solskiy <ivan@decoded.org.ua>
2017-11-01 03:46:21 +0200

    prev and next sliding - not perfect, need to fix styles

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,7 @@
-var btn_prev = document.getElementById('prev');
-var btn_next = document.getElementById('next');
-var slides = document.getElementsByClassName('image-slide');
-var i = 0;
+// var btn_prev = document.getElementById('prev');
+// var btn_next = document.getElementById('next');
+// var slides = document.getElementsByClassName('image-slide');
+// var i = 0;
 
 
 $(document).ready(function(){
@@ -9,13 +9,16 @@ $(document).ready(function(){
 
   owl.owlCarousel({
     stagePadding: '50',
-    //smartSpeed:1500,
+    touchDrag  : false,
+    mouseDrag  : false,
+    smartSpeed:1000,
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
     loop:false,
     margin: 15,
     nav:true,
-    responsive:{
+    navText:['<div id="prev">Попередній<br>проект</div>','<div id="next">Наступний<br>проект</div>'],
+    responsive: {
         0:{
             items:1
         },
@@ -28,34 +31,81 @@ $(document).ready(function(){
     },
     onInitialize: createAnimations,
     ///onDragged:callback
-    onTranslate:createAnimations,
+    // onTranslate:createAnimations,
 
   });
+
+  // $('.owlNextBtn').click(function() {
+    
+  //   owl.trigger('next.owl.carousel');
+
+
+  // })
+  // // Go to the previous item
+  // $('.owlPrevBtn').click(function() {
+  //     // With optional speed parameter
+  //     // Parameters has to be in square bracket '[]'
+  //     // owl.trigger('prev.owl.carousel', [300]);
+  //     owl.trigger('prev.owl.carousel');
+  // })
+
+  owl.on('change.owl.carousel', function(event) {
+
+    var $currentItem = $('.owl-item').eq(event.item.index);
+    var $itemToShow = $('.owl-item').eq(event.property.value);
+    // var $itemToShow = $('.owl-item').eq(event.item.index);
+    
+    if(event.item.index < event.property.value){
+      $itemToShow.addClass('animated owl-animated-in fadeNext');
+      // $currentItem.addClass('animated owl-animated-out');
+      console.log('next');      
+    }
+    
+    if(event.item.index > event.property.value){
+      console.log('prev');
+      $currentItem.addClass('animated owl-animated-in fadePrev');
+    }
+
+  });
+
+  owl.on('translated.owl.carousel', function(event) {
+
+    $('.owl-item').removeClass('animated fadePrev fadeNext fadeOut fadeIn owl-animated-out owl-animated-in');
+
+  });
+
+
 });
 
 
-btn_next.addEventListener('click', function() {
-  //images[i].classList.remove('active');
-  i--;
-  if(i < 0) {
-    i = images.length - 1;
-  }
-  //images[i].classList.add('active');
+// btn_next.addEventListener('click', function() {
+//   //images[i].classList.remove('active');
+//   i--;
+//   if(i < 0) {
+//     i = images.length - 1;
+//   }
+//   //images[i].classList.add('active');
 
-});
+// });
 
-btn_prev.addEventListener('click', function() {
+// btn_prev.addEventListener('click', function() {
 
-  //images[i].classList.remove('active');
-  i++;
-  if(i >= images.length) {
-    i = 0;
-  }
-  //images[i].classList.add('active');
-  //TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
+//   //images[i].classList.remove('active');
+//   i++;
+//   if(i >= images.length) {
+//     i = 0;
+//   }
+//   //images[i].classList.add('active');
+//   //TweenMax.fromTo(".active", 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)", zIndex:100 }, {scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)"});
 
-});
+// });
 
+// Fired after current slide has been changed
+// owl.on('changed.owl.carousel', function(event) {
+//   var $currentItem = $('.owl-item', owl).eq(event.item.index);
+//   var $elemsToanim = $currentItem.find("[data-animation-in]");
+//   setAnimation ($elemsToanim, 'in');
+// })
 
 function createAnimations(event) {
   
@@ -68,6 +118,7 @@ function createAnimations(event) {
   event.item.size
   );
   // TweenMax.fromTo('.owl-item .active', 1, {scale: 1.1, boxShadow:"25px 0px 10px 20px rgba(0,0,0,0.59)"}, {scale:1,  boxShadow:"0px 0px 0px 0px rgba(0,0,0,0.59)" });
+
 }
 
 //'owl-item active'
