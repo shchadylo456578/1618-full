c867869035e795fba5382cd2bea390c98142d82d
shchadylo456578 <tarasdidg@gmail.com>
2017-11-14 12:46:12 +0200

    add pages_article for word-press

--- src/js/change-words.js
+++ src/js/change-words.js
@@ -1,16 +1,55 @@
-var words = [
-  'Ви зростаєте <br/> Ми втілюємо',
-  'Ви відпочиваєте <br/> Ми працюємо',
-  'Ви спите <br/> Ми ремонтуємо',
-  'Ви радієте <br/> Ми будуємо'
+var wordsfirsr = [
+  ' зростаєте',
+  ' відпочиваєте',
+  ' спите',
+  ' радієте',
+  ' мрієте'
 ];
 
 $(function () {
   count = 0;
   setInterval(function () {
     count++;
-    $("#changeWords").fadeOut(500, function () {
-      $(this).html(words[count % words.length]).fadeIn(500);
+    $("#changeWords_1").fadeOut(500, function () {
+      $(this).html(wordsfirsr[count % wordsfirsr.length]).fadeIn(500);
     });
   }, 3000);
 });
+
+
+var wordssecond = [
+  ' втілюємо',
+  ' працюємо',
+  ' ремонтуємо',
+  ' будуємо',
+  ' проектуємо'
+];
+
+$(function () {
+  count = 0;
+  setInterval(function () {
+    count++;
+    $("#changeWords_2").fadeOut(500, function () {
+      $(this).html(wordssecond[count % wordssecond.length]).fadeIn(500);
+    });
+  }, 3000);
+});
+
+
+
+// var words = [
+//   'Ви зростаєте <br/> Ми втілюємо',
+//   'Ви відпочиваєте <br/> Ми працюємо',
+//   'Ви спите <br/> Ми ремонтуємо',
+//   'Ви радієте <br/> Ми будуємо'
+// ];
+//
+// $(function () {
+//   count = 0;
+//   setInterval(function () {
+//     count++;
+//     $("#changeWords").fadeOut(500, function () {
+//       $(this).html(words[count % words.length]).fadeIn(500);
+//     });
+//   }, 3000);
+// });
